983d252cc0b54bd2f0667959a44f06d987d19399
pauldidonatojr <pjdidonatojr@gmail.com>
2023-07-04 13:44:39 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ModalProvider } from './context/modal_context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ModalProvider>
+
         <App />
-    </ModalProvider>
 )
 
 // If you want to start measuring performance in your app, pass a function
